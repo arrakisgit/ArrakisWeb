360a37508972d720e10efbe31778895e94288c30
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-08-10 04:51:07 +0200

    Dev Channels

--- Channels.php
+++ Channels.php
@@ -13,6 +13,9 @@ class Arte extends ScrappingCURL implements IChannel
 	private $URL_MOST_VIEWED;
 	private $URL_LAST_CHANCE;
 	private $ArrayCategories;
+	private $ResultJSON;
+	private $ArrayShows;
+	private $ArrayEpisodes;
 	
 	public function __construct()
 	{
@@ -21,40 +24,75 @@ class Arte extends ScrappingCURL implements IChannel
 		$this->URL_MOST_VIEWED='http://www.arte.tv/papi/tvguide/videos/plus7/program/F/L2/ALL/ALL/-1/VIEWS/0/0/DE_FR.json';
 		$this->URL_LAST_CHANCE='http://www.arte.tv/papi/tvguide/videos/plus7/program/F/L2/ALL/ALL/-1/LAST_CHANCE/0/0/DE_FR.json';
 		$this->ArrayCategories=Array();
+		$this->ArrayShows=Array();
+		$this->ArrayEpisodes=Array();
+		$this->ResultJSON = parent::Func_Get_Source_Code_From_JSON($this->URL_CATEGORIES);
 	}
 	
 	public function Categories()
 	{
-		$ResultJSON = parent::Func_Get_Source_Code_From_JSON($this->URL_CATEGORIES);
-		foreach($ResultJSON->programFRList as $programItem)
+		foreach($this->ResultJSON->programFRList as $programItem)
 		{
 			foreach($programItem->VDO as $VDO_item)
 			{
-				if(array_key_exists($VDO_item->VCH->label,$this->ArrayCategories)==false)
-				{
-					$ArrayItem=Array();
-					
-					if(array_key_exists($programItem->PID, $ArrayItem)==false)
+				if(array_key_exists($VDO_item->VCH->label, $this->ArrayCategories)==false)
 					{
-						$ArrayItem[$programItem->PID]=$VDO_item->VCH->V7T;
-						$this->ArrayCategories[$VDO_item->VCH->label]=$ArrayItem;
+						$this->ArrayCategories[$VDO_item->VCH->label]=$VDO_item->VCH->channelID;
 					}
 					
-				}
-				else 
+			}
+		}
+		return $this->ArrayCategories;
+	}
+	
+	public function Shows($categorySelected)
+	{
+		foreach($this->ResultJSON->programFRList as $programItem)
+		{
+			foreach($programItem->VDO as $VDO_item)
+			{
+				if($VDO_item->VCH->label==$categorySelected)
 				{
-					if(array_key_exists($programItem->PID, $ArrayItem)==false)
+					if(array_key_exists($programItem->PID, $this->ArrayShows)==false)
 					{
-						$ArrayItem=$this->ArrayCategories[$VDO_item->VCH->label];
-						$ArrayItem[$programItem->PID]=$VDO_item->VCH->V7T;
-						$this->ArrayCategories[$VDO_item->VCH->label]=$ArrayItem;
+						$this->ArrayShows[$programItem->PID]=$programItem->TIT;
 					}
+				}	
+			}
+		}
+		return $this->ArrayShows;
+	}
+	
+	public function StreamUrl($showSelected)
+	{
+		foreach($this->ResultJSON->programFRList as $programItem)
+		{
+			if ($programItem->PID=$showSelected)
+			{
+				return File_Video_Url($programItem->VDO->videoStreamUrl);
 					
+			}
+		}
+	}
+	
+	private function File_Video_Url($stream_url)
+	{
+		$jsonresult=parent::Func_Get_Source_Code_From_JSON($stream_url);
+		foreach($jsonresult->video as $videos)
+		{
+			foreach($videos->VSR as $VSR_Item)
+			{
+				if($VSR_Item->VFO=='HBBTV' && $VSR_Item->VQU=='HQ')
+				{
+					return $VSR_Item->VUR;
 				}
 			}
 		}
 	}
 		
 }
+
+		
+
 
 ?>
--- Interfaces.php
+++ Interfaces.php
@@ -13,11 +13,11 @@ interface IChannel
 	
 	public function Categories();
 	public function Shows($categorySelected);
-	public function Episodes($categorySelected,$showSelected);
+	public function StreamUrl($showSelected);
 	public function Live();
-	public function Descriptions($categorySelected,$showSelected,$EpisodeSelected);
-	public function Images($categorySelected,$showSelected,$EpisodeSelected);
-	public function Durations($categorySelected,$showSelected,$EpisodeSelected);
+	public function Descriptions($showSelected);
+	public function Images($showSelected);
+	public function Durations($showSelected);
 	
 }
 ?>
