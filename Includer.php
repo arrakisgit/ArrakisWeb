94ca8472c26fe0d54175630ff7fa50a4535c40be
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-04 22:26:45 +0200

    Add includer

---
+++ Includer.php
@@ -0,0 +1,16 @@
+<?php
+/*Auteur      : Amine El Ouazzani
+ *Projet      : Arrakis
+ *Date        : 01/09/2016 10:25 PM
+ *Licence     : GNU GPL v3
+ *Description : Incluseur de librairies
+ *git         : https://github.com/arrakisgit/ArrakisWeb.git
+ */
+
+include 'Channels.php';
+include 'Interfaces.php';
+include 'init_scrapping.php';
+include 'Utils_Functions.php';
+
+
+?>
