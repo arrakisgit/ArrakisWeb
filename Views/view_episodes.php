16a8f21c484bab2cd3dc1d5e99e0cc594e3ec454
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-11 18:17:30 +0200

    correction

--- Views/view_episodes.php
+++ Views/view_episodes.php
@@ -1,4 +1,32 @@
+<html><head>
+
+<title>Video/Audio</title>
+
+<link rel="stylesheet" type="text/css" href="http://192.168.1.20/ArrakisWeb_Lib/libs_css/style.css">
+<style type="text/css">
+
+</style>
+
+<script src="http://192.168.1.20/ArrakisWeb_Lib/libs_js/jquery.js"></script>
+
+<script src="http://192.168.1.20/ArrakisWeb_Lib/libs_js/jLecteur.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+	$('video').videoPlayer({
+		'playerWidth' : 0.95,
+		'videoClass' : 'video'	
+	});
+});
+
+</script>
+
+</head>
+<body>
+
 <?php 
-echo "nooooo";
-echo $urlEpisode;
+	echo "<div class='container'><video width='700' height='400'><source src='".$urlEpisode."' type='video/mp4'></video></div>";
 ?>
+
+</body></html>
