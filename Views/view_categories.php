75b06453efb1f2b637cbb6ff06b4e93934ef9d8c
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-11 05:55:29 +0200

    test

--- Views/view_categories.php
+++ Views/view_categories.php
@@ -6,10 +6,10 @@
  *Description : View CodeIgniter manage les catégories des chaînes
  *git         : https://github.com/arrakisgit/ArrakisWeb.git
  */
-echo 'test';
+echo $Channel.'\n';
 foreach ($ArrayCat as $labelCat=>$channelID)
 {
-	echo "<br>".$labelCat."--------->".$channelID."</br>";
+	echo "<br><a href='http://192.168.1.20/ArrakisWeb/index.php/Shows/index/".$Channel.'_'.$channelID.'>'.$labelCat."</a></br>";
 }
 
 ?>
