c738ea2dfeb46e108f37b0c9aa2d2e21bdbad77c
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-11 06:21:29 +0200

    correction script

--- script_shell/config_arrakisweb_codeigniter.sh
+++ script_shell/config_arrakisweb_codeigniter.sh
@@ -10,5 +10,6 @@ cd /var/www/html/ArrakisWeb/application/ArrakisWeb
 sudo cp -R lib_php /var/www/html/ArrakisWeb/application/controllers
 cd Controllers
 sudo cp -R Categories.php /var/www/html/ArrakisWeb/application/controllers
+sudo cp -R Shows.php /var/www/html/ArrakisWeb/application/controllers
 cd /var/www/html/ArrakisWeb/application/ArrakisWeb/Views
 sudo cp -R *.php /var/www/html/ArrakisWeb/application/views
