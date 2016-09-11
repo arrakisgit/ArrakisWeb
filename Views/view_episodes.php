1b22d0f4c9dc9f5eccf333c181845f7316db5cd7
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-11 10:24:01 +0200

    kkkkk

--- Views/view_episodes.php
+++ Views/view_episodes.php
@@ -1,35 +1,3 @@
-<html><head>
-
-<title>Video/Audio</title>
-
-<link rel="stylesheet" type="text/css" href="http://192.168.1.20/ArrakisWeb_Lib/libs_css/style.css">
-<style type="text/css">
-
-</style>
-
-<script src="http://192.168.1.20/ArrakisWeb_Lib/libs_js/jquery.js"></script>
-
-<script src="http://192.168.1.20/ArrakisWeb_Lib/libs_js/jLecteur.js"></script>
-
-<script type="text/javascript">
-
-$(document).ready(function() {
-	$('video').videoPlayer({
-		'playerWidth' : 0.95,
-		'videoClass' : 'video'	
-	});
-});
-
-</script>
-
-</head>
-<body>
-
-<div class="container">
-	<video width="700" height="400">
-	<source src='citrons.mp4' type='video/mp4'>
-	</video>
-</div>
-
-
-</body></html>
+<?php 
+echo $urlEpisode;
+?>
