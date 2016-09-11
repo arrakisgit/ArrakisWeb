5b6a07bc422310909d3aee9ae2f146e1edcccb3b
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-11 09:22:05 +0200

    ???

--- lib_php/Channels.php
+++ lib_php/Channels.php
@@ -123,13 +123,14 @@ class Arte extends ScrappingCURL implements IChannel
 	public function File_Video_Url($stream_url)
 	{
 		$jsonresult=parent::Func_Get_Source_Code_From_JSON_SESSION($stream_url);
-		foreach($jsonresult['video']['VSR'][1]['VFO'] as $vfo)
-		{
-					if (($vfo['VQU']=="HD") && ($vfo=="HBBTV"))
-					{
+		//foreach($jsonresult['video']['VSR']['VFO'] as $vfo)
+		//{
+			$vfo=$jsonresult['video']['VSR']['VFO'][5];
+					//if (($vfo['VQU']=="HD") && ($vfo=="HBBTV"))
+					//{
 						return $vfo['VUR'];
-					}
-		}
+					//}
+		//}
 		
 	}
 		
