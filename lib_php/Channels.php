b46f964e0d384094642c81344466c9c8d0680118
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-09-26 22:05:30 +0200

    FranceTV

--- lib_php/Channels.php
+++ lib_php/Channels.php
@@ -210,9 +210,9 @@ class FranceTV extends ScrappingCURL implements IChannel
 	public function Categories()
 	{
 		$ARRAY_CATEGORIES=Array();
-		/*foreach($this->JSON_RESULT_CATEGORIES_FRANCETV['categories'][0] as $categories)
+		foreach($this->JSON_RESULT_CATEGORIES_FRANCETV['categories'][0] as $categories)
 		{
-			foreach($categories['genres'] as $genres)
+			foreach($categories['genres'][0] as $genres)
 			{
 				foreach($genres['genre'] as $genre)
 				{
@@ -231,9 +231,9 @@ class FranceTV extends ScrappingCURL implements IChannel
 			{
 				$ARRAY_CATEGORIES[$title]=$title;
 			}
-		}*/
+		}
 		
-		return var_dump($this->JSON_RESULT_CATEGORIES_FRANCETV);//$ARRAY_CATEGORIES;
+		return $ARRAY_CATEGORIES;
 	}
 	
 	public function Shows($categorySelected)
