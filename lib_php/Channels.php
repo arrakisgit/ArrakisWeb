b365da7ca0c191db55136ef1f509866f04c53e06
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-18 00:16:13 +0200

    test

--- lib_php/Channels.php
+++ lib_php/Channels.php
@@ -270,7 +270,7 @@ class FranceTV extends ScrappingCURL implements IChannel
 				}
 			}
 		}
-		return $this->FRANCETV_SHOWS;
+		return var_dump($this->FRANCETV_SHOWS);
 	}
 	
 	public function Episodes($showSelected)
