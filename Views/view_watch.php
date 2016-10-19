b62d90e637fd175e13cd6631a6bb66dc0c2cb966
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-19 23:17:42 +0200

    add clappr

--- Views/view_watch.php
+++ Views/view_watch.php
@@ -2,44 +2,34 @@
 
 <title>Video/Audio</title>
 
-<link rel="stylesheet" type="text/css" href="http://192.168.0.18/ArrakisWeb_Lib/libs_css/style.css">
-<style type="text/css">
-
-</style>
-
-<script src="http://192.168.0.18/ArrakisWeb_Lib/libs_js/jquery.js"></script>
-
-<script src="http://192.168.0.18/ArrakisWeb_Lib/libs_js/jLecteur.js"></script>
-
 <script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
 
-<script type="text/javascript">
 
-$(document).ready(function() {
-	$('video').videoPlayer({
-		'playerWidth' : 0.95,
-		'videoClass' : 'video'	
-	});
-});
-
-</script>
 
 </head>
 <body>
 
+
+<div id="player"></div>
+    <script>
+      var player = new Clappr.Player({
+        source: <?php echo "'".$urlEpisode."',"?>
+        plugins: [ChromecastPlugin],
+        parentId: '#player',
+        chromecast: {
+          appId: '9DFB77C0',
+          contentType: 'video/mp4',
+          media: {
+            type: ChromecastPlugin.Movie,
+            title: 'Awesome Hot Air Balloon Slackline',
+            subtitle: 'You won\'t get much closer to Skylining than this!'
+          }
+        }
+      });
+    </script>
 <?php 
 
-	/*if($typeVid=="mp4")
-	{
-		echo "<div class='container'><video width='700' height='400'><source src='".$urlEpisode."' type='video/mp4'></video></div>";
-		echo "<div><br>".$id.'|'.$Shows."|".$urlEpisode."</br></div>";
-	}
-	elseif ($typeVid=="m3u8")
-	{*/
-		echo "<div id='player'></div>";
-  		echo "<script>";
-    	echo "var player = new Clappr.Player({source: '".$urlEpisode."', parentId: '#player'});";
-  		echo "</script>";
 		//echo "<div class='container'><video width='700' height='400'><source src='".$urlEpisode."' type='application/x-mpegURL'></video></div>";
 		echo "<div><br>".$id.'|'.$Shows."|".$urlEpisode."</br></div>";
 	//}
