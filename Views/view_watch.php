22f249e640f37b6049f268ee7498dc8cb1250e61
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-19 22:55:36 +0200

    test clappr for hls

--- Views/view_watch.php
+++ Views/view_watch.php
@@ -11,6 +11,8 @@
 
 <script src="http://192.168.0.18/ArrakisWeb_Lib/libs_js/jLecteur.js"></script>
 
+<script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+
 <script type="text/javascript">
 
 $(document).ready(function() {
@@ -34,8 +36,12 @@ $(document).ready(function() {
 	}
 	elseif ($typeVid=="m3u8")
 	{
-		echo "<div class='container'><video width='700' height='400'><source src='".$urlEpisode."' type='application/x-mpegURL'></video></div>";
-		echo "<div><br>".$id.'|'.$Shows."|".$urlEpisode."</br></div>";
+		echo "<div id='player'></div>";
+  		echo "<script>";
+    	echo "var player = new Clappr.Player({source: '".$urlEpisode."', parentId: '#player'});";
+  		echo "</script>";
+		//echo "<div class='container'><video width='700' height='400'><source src='".$urlEpisode."' type='application/x-mpegURL'></video></div>";
+		//echo "<div><br>".$id.'|'.$Shows."|".$urlEpisode."</br></div>";
 	}
 	
 ?>
