416f4097fb6dfb0a4d35e453ccc62cee8b5a73e6
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-20 00:57:11 +0200

    test

--- Views/view_watch.php
+++ Views/view_watch.php
@@ -15,18 +15,17 @@
         var player = new Clappr.Player({
             							source: <?php echo "'".$urlEpisode."',\n"?>
         								parentId: "#player", 
-            							plugins: { playback: [Clappr.HLS],
-												   playback: [ChromecastPlugin]},
+            							plugins: {'core': [ChromecastPlugin],
+                							playback: [Clappr.HLS]},
             							baseUrl: '/latest',
             							chromecast: {
-            							          appId: '9DFB77C0',
-            							          contentType: 'video/mp4',
-            							          media: {
-            							            type: ChromecastPlugin.Movie,
-            							            title: 'Awesome Hot Air Balloon Slackline',
-            							            subtitle: 'You won\'t get much closer to Skylining than this!'
-            							          }
-            							        }});
+            						          appId: '9DFB77C0',
+            						          media: {
+            						            title: 'Awesome Hot Air Balloon Slackline',
+            						            subtitle: 'You won\'t get much closer to Skylining than this!'
+            						          }
+            						        }
+            							});
       
     </script>
 <?php 
