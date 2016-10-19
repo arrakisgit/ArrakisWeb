8d0090351325423591fc3c74964fd5bfb5282b1c
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-19 23:45:16 +0200

    test

--- Views/view_watch.php
+++ Views/view_watch.php
@@ -3,7 +3,6 @@
 <title>Video/Audio</title>
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
-<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
 
 </head>
 <body>
@@ -11,29 +10,8 @@
 
 <div id="player"></div>
     <script>
-      var typeVid = <?php echo "\"".$typeVid."\";\n" ?>
-      if (typeVid=="mp4")
-      {
-            alert('ok');
-	      	var player = new Clappr.Player({
-	        source: <?php echo "'".$urlEpisode."',\n"?>
-	        plugins: [ChromecastPlugin],
-	        parentId: '#player',
-	        chromecast: {
-	          appId: '9DFB77C0',
-	          contentType: 'video/mp4',
-	          media: {
-	            type: ChromecastPlugin.None,
-	            title: 'Awesome Hot Air Balloon Slackline',
-	            subtitle: 'You won\'t get much closer to Skylining than this!'
-	          }
-	        }
-	      });
-      }
-      else
-      {
-    	  var player = new Clappr.Player({source: source: <?php echo "'".$urlEpisode."',"?> parentId: "#player"});
-      }
+        var player = new Clappr.Player({source: source: <?php echo "'".$urlEpisode."',"?> parentId: "#player"});
+      
     </script>
 <?php 
 
