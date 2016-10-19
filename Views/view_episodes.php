db0ee06824a078d985affe889223265dc4e6cf74
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-19 21:43:53 +0200

    correction

--- Views/view_episodes.php
+++ Views/view_episodes.php
@@ -11,7 +11,7 @@
 		{
 			//
 			//echo $Episodes;
-			echo "<br><a href='http://192.168.0.18/ArrakisWeb/index.php/Episodes/index/".$Channels."_".$Shows."_".$labelShow."'>".$showID."</a></br>";
+			echo "<br><a href='http://192.168.0.18/ArrakisWeb/index.php/Watch/index/".$Channels."_".$Shows."_".$labelShow."'>".$showID."</a></br>";
 		}
 		//echo "<div class='container'><video width='700' height='400'><source src='".$Episode."' type='application/x-mpegURL'></video></div>";
 	
