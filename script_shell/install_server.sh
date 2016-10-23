7fa8e26d0a056f8c3872515e716408a0fde95d54
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-10-23 17:17:07 +0200

    ajout composer et config mastermind/html5

--- script_shell/install_server.sh
+++ script_shell/install_server.sh
@@ -6,3 +6,7 @@ sudo chmod -R 770 /var/www/html/
 sudo apt-get install php5 mysql-server libapache2-mod-php5 php5-mysql php5-curl
 sudo rm /var/www/html/index.html
 sudo service apache2 start
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php
+sudo php -r "unlink('composer-setup.php');"
