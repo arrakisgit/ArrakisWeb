9ea55eb625801d5f6b631152a3a1e36a2a00831b
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-11-04 01:33:14 +0100

    idem

--- script_shell/mount_smb.sh
+++ script_shell/mount_smb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo mount.cifs $0 -o /var/www/html/ArrakisWeb/SRVNAS_WEB -o guest,workgroup=WORKGROUP
+sudo mount.cifs $1 -o /var/www/html/ArrakisWeb/NASSRV_WEB -o guest,workgroup=WORKGROUP
