12c0a13dee6a6a874031165814118fc0a0f7615e
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-02-19 05:16:25 +0100

    test converter

--- Views/view_watch.php
+++ Views/view_watch.php
@@ -19,6 +19,7 @@
     	$js_ffmpeg_command='-i input.webm -vf showinfo -strict -2 output.mp4';
     	//echo "<script type='text/javascript'>\n";
     	echo "retrieveVideo('".$urlEpisode."');\n";
+    	echo "initWorker();\n";
     	echo "runCommand('".$js_ffmpeg_command."');\n";
     	//echo "</script>\n";
     }
