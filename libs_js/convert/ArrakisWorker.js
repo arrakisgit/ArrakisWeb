ecf82861701033ef448ec494ef9288ce1ec97fb4
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-03-02 22:56:31 +0100

    study ffmpeg.js

--- libs_js/convert/ArrakisWorker.js
+++ libs_js/convert/ArrakisWorker.js
@@ -1,4 +1,4 @@
-importScripts('/ArrakisWeb_Lib/libs_js/convert/build/ffmpeg-all-codecs.js');
+importScripts('/ArrakisWeb_Lib/libs_js/convert/build/ffmpeg.js');
 
 function print(text) {
   postMessage({
