44bd4007a5a4dfe60737e7d3ec837222d554316d
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-03-12 23:33:13 +0100

    correction path

--- lib_php/init_scrapping.php
+++ lib_php/init_scrapping.php
@@ -237,7 +237,7 @@ class ScrappingCURL
 	public function SendCallArrakisServices($urlFile)
 	{
 		
-		$file_name_with_full_path=$urlFile;
+		$file_name_with_full_path=str_replace('http://192.168.0.18/','/var/www/html/',$urlFile);
 		if (function_exists('curl_file_create')) 
 		{ // php 5.6+
 			$cFile = curl_file_create($file_name_with_full_path);//,'video/avi','test');
