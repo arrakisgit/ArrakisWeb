315824bd9e6aa31dc6fe49835501005e498e17c7
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-07-01 22:45:37 +0200

    test local VPS

--- lib_php/init_scrapping.php
+++ lib_php/init_scrapping.php
@@ -8,7 +8,7 @@
  *git         : https://github.com/arrakisgit/ArrakisWeb.git
  */
 
-require '/vendor/autoload.php';
+require '../vendor/autoload.php';
 use Masterminds\HTML5;
 use GuzzleHttp\Client;
 use Masterminds\HTML5\Parser\DOMTreeBuilder;
