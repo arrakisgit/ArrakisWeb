12236b3dbc4f831457ec64a0fdafcae45a21bc4b
arrakis-dev <elouazzani.amine@gmail.com>
2018-02-04 17:42:51 +0100

    test

--- Views/view_episodes.php
+++ Views/view_episodes.php
@@ -16,12 +16,12 @@ $SERVEUR_PATH="http://".$_SERVER['HTTP_HOST'].'/ArrakisWeb/';
 			//echo $Episodes;
 			if ($Channels!='Kodi')
 			{
-				echo "<br><a href='".$SERVEUR_PATH."index.php/Watch/index/".$Channels."_".$Categories."_".$Shows."_".$labelShow."'>".$showID."</a></br>";
+				echo "<br><a href='".$SERVEUR_PATH."index.php/Watch/index/".$Channels."_".$Categories."_".$Shows."_".$showID."'>".$labelShow."</a></br>";
 		
 			}
 			else
 			{
-				echo "<br><a href='".$SERVEUR_PATH."index.php/Watch/index/".$Channels."_".$Categories."_".$Shows."_".$Seasons."_".$labelShow."'>".$showID."</a></br>";
+				echo "<br><a href='".$SERVEUR_PATH."index.php/Watch/index/".$Channels."_".$Categories."_".$Shows."_".$Seasons."_".$showID."'>".$labelShow."</a></br>";
 			}
 		}
 		//echo "<div class='container'><video width='700' height='400'><source src='".$Episode."' type='application/x-mpegURL'></video></div>";
